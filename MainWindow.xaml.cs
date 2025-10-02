8d01788c8b4d0f94efc37d4d864f5e95aeaa2e41
Yeghor Danyliuk <egordan74@gmail.com>
2025-10-02 15:55:44 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PKPZlab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            char[] input = Input.Text.ToCharArray();
+            int rs = 0;
+            int ks = 0;
+            int ts = 0;
+            foreach (char c in input)
+            {
+                switch (c)
+                {
+                    case 'r':
+                        rs++;
+                        break;
+                    case 'k':
+                        ks++;
+                        break;
+                    case 't':
+                        ts++;
+                        break;
+                }
+            }
+            r_counter.Content = rs;
+            k_counter.Content = ks;
+            t_counter.Content = ts;
+        }
+
+        private void Input_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Input.Text == "Random text")
+            {
+                Input.Clear();
+            }
+        }
+    }
+}
